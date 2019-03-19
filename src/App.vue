72fec05214187806f4106ae23f5a5b9b7e7788ca
MauroNava <mauronavaluevanos63@gmail.com>
2019-03-18 20:35:41 -0600

    Bootstrap, Pug, Sass and Styles initialized

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+  div#app.d-flex.flex-column
+    router-view/
 </template>
 
 <script>
@@ -11,13 +9,10 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import 'assets/scss/base';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh !important;
 }
 </style>
---
+++ src/components/Timer.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  div.h-100#timer
+    p#clock {{ time }}
+</template>
+
+<script>
+export default {
+  name: 'Timer',
+  data: () => (
+    {
+      time: '00'
+    }
+  )
+}
+</script>
+
+<style lang="scss" scoped>
+#timer {
+  transform: translate(0);
+  #clock {
+    transform: translate(-50%, -50%);
+    position: absolute;
+    font-size: 2rem;
+    left: 50%;
+    top: 50%;
+  }
+}
+</style>
