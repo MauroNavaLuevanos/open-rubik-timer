06083040fb3d68f366cd5cb87b0bb1bab9ef7c85
MauroNavaLuevanos <mauronavaluevanos63@gmail.com>
2019-04-22 00:26:15 -0500

    Stats Initialized

---
+++ src/components/views/Stats.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div#stats.h-100
+</template>
+
+<script>
+import {
+  mapState
+} from 'vuex'
+
+export default {
+  name: 'Stats',
+  computed: mapState({times: 'timesList'})
+}
+</script>
